86539eb658a8809645d851f089fac76965fd6065
filippo_20tab <filippo@20tab.com>
2017-07-20 15:08:32 +0200

    First release

---
+++ README.md
@@ -0,0 +1,104 @@
+# Definition
+
+This is a step-by-step guide to install and configure the uWSGI server in _emperor_ mode and with multiple language support. It is mainly intended for macOS environments but, excluding the parts related to bonjour and `launchd`, it can be adapted to other platforms.
+
+# Configuration
+
+Please make sure to read each of the following sections, and complete them in the specified order.
+
+### uWSGI
+
+uWSGI will be installed from its sources with the support for multiple languages, then a plugin will be built for each language to be supported:
+- get uwsgi latest sources from [here](http://uwsgi-docs.readthedocs.io/en/latest/Download.html);
+- extract the files in a local directory;
+- open a terminal and `cd` into the uwsgi sources directory;
+- build the uwsgi binary:
+```sh
+make PROFILE=nolang
+```
+- generate the plugins, being sure to already have installed, for each plugin, its corresponding binary (e.g. `python3.5` or `python27`). In the case of various Python versions, execute:
+```sh
+PYTHON=python2.7 ./uwsgi --build-plugin "plugins/python python27"
+PYTHON=python3.6 ./uwsgi --build-plugin "plugins/python python36"
+```
+- create a local directory where you intend to collect all your uWSGI plugins (e.g. `~/uwsgi/plugins/`);
+- copy **all** the plugins to the directory you have just created:
+```sh
+cp python27_plugin.so ~/uwsgi/plugins/
+cp python36_plugin.so ~/uwsgi/plugins/
+```
+- install the compiled uWSGI binary, e.g.:
+```sh
+cp uwsgi /usr/local/bin/
+```
+
+### Bonjour
+
+This part is only meant for macOS and makes it possible to assign a custom domain to each project (e.g. `my_new_project.local/`):
+- clone or download the repository and `cd` in its directory;
+- compile the bonjour uWSGI plugin:
+```sh
+uwsgi --build-plugin uwsgi-bonjour
+```
+- copy the plugin to the uWSGI plugins directory:
+```sh
+cp bonjour_plugin.so ~/uwsgi/plugins/
+```
+Code is taken from Unbit uwsgi-bonjour, for further information check [here](https://github.com/unbit/uwsgi-bonjour).
+
+### Emperor
+
+For more information on uWSGI _emperor_ mode refer to the [official documentation](http://uwsgi-docs.readthedocs.io/en/latest/Emperor.html). Before configuring uWSGI:
+- create a directory where to collect all the projects .ini configuration files (e.g. `~/uwsgi/vassals/`);
+- create a directory where to collect all the projects log files (e.g. `~/uwsgi/logs/`)
+
+Provided `emperor.ini` contains a sample configuration to run the uWSGI server in _emperor_ mode. Customize it with the paths to your vassals and plugins directories and to the emperor log file, plus any other setting/option you wish to run the emperor with. Then:
+- move the customised emperor configuration file to a convenient location:
+```sh
+cp emperor.ini ~/uwsgi/
+```
+Be careful not to put the emperor configuration file into the vassals configuration files directory.
+
+### Vassals
+
+The emperor needs to be told the language of each project, therefore make sure to indicate the corresponding plugin in each vassal configuration file:
+```ini
+plugin = python35
+```
+##### Bonjour (macOS)
+
+In order to assign a custom domain to a project (e.g. `my_new_project.local/`), also include the following lines in the corresponding vassal configuration file:
+```ini
+plugin = bonjour
+bonjour-register = name=%(project_name).local,cname=localhost
+socket = 127.0.0.1:0
+subscribe-to = 127.0.0.1:5005:%(project_name).local
+```
+
+# Execution
+
+The emperor can now be launched both manually and automatically.
+
+### Manual
+
+This is straightforward, just launch uWSGI as superuser and with the emperor configuration file:
+```sh
+sudo uwsgi ~/uwsgi/emperor.ini
+```
+
+### Automatic (macOS)
+
+Provided `it.unbit.uwsgi.emperor.plist` is a sample property list file that instructs `launchd` to run the emperor as a daemon. Customize it with the paths to your `uwsgi` binary (e.g. `/usr/local/bin/uwsgi`) and to the configuration file (`~/uwsgi/emperor.ini`). Then move, as superuser, the property list file to its location, load it and start it:
+```sh
+sudo cp it.unbit.uwsgi.emperor.plist /Library/LaunchDaemons/
+sudo launchctl load /Library/LaunchDaemons/it.unbit.uwsgi.emperor.plist
+sudo launchctl start /Library/LaunchDaemons/it.unbit.uwsgi.emperor.plist
+```
+In case you need to stop the daemon:
+```sh
+sudo launchctl stop /Library/LaunchDaemons/it.unbit.uwsgi.emperor.plist
+```
+In case you need to unload the daemon:
+```sh
+sudo launchctl unload /Library/LaunchDaemons/it.unbit.uwsgi.emperor.plist
+```
---
+++ uwsgi-emperor-mode/uwsgi-bonjour/bonjour.c
@@ -0,0 +1,185 @@
+#include <uwsgi.h>
+#include <dns_sd.h>
+
+extern struct uwsgi_server uwsgi;
+
+static struct uwsgi_bonjour {
+	struct uwsgi_string_list *record;
+} ubonjour;
+
+static DNSServiceRef bonjour_sdref = NULL;
+
+static struct uwsgi_option bonjour_options[] = {
+	{"bonjour-register", required_argument, 0, "register a record in the Bonjour service (default: register a CNAME for the local hostname)", uwsgi_opt_add_string_list, &ubonjour.record, 0},
+	{"bonjour-register-record", required_argument, 0, "register a record in the Bonjour service (default: register a CNAME for the local hostname)", uwsgi_opt_add_string_list, &ubonjour.record, 0},
+	{"bonjour-rr", required_argument, 0, "register a record in the Bonjour service (default: register a CNAME for the local hostname)", uwsgi_opt_add_string_list, &ubonjour.record, 0},
+	UWSGI_END_OF_OPTIONS
+};
+
+// fake function
+static void dnsCallback(DNSServiceRef sdRef __attribute__((unused)),
+            DNSRecordRef RecordRef __attribute__((unused)),
+            DNSServiceFlags flags __attribute__((unused)),
+            DNSServiceErrorType errorCode __attribute__((unused)),
+            void *context __attribute__((unused))) {
+}
+
+// convert a c string to a dns string (each chunk is prefix with its size in uint8_t) + \0 suffix
+static struct uwsgi_buffer *to_dns(char *name, size_t len) {
+	uint8_t chunk_len = 0;
+	size_t i;
+	struct uwsgi_buffer *ub = uwsgi_buffer_new(len);
+	if (!ub) goto error;
+	char *ptr = name;
+
+	for(i=0;i<len;i++) {
+		if (name[i] == '.') {
+			if (uwsgi_buffer_u8(ub, chunk_len)) goto error;
+			if (uwsgi_buffer_append(ub, ptr,chunk_len)) goto error;
+			ptr = name + i + 1;
+			chunk_len = 0;
+		}
+		else {
+			chunk_len++;	
+		}
+	}
+
+	if (chunk_len > 0) {
+		if (uwsgi_buffer_u8(ub, chunk_len)) goto error;
+                if (uwsgi_buffer_append(ub, ptr,chunk_len)) goto error;
+	}
+
+        if (uwsgi_buffer_append(ub, "\0", 1)) goto error;
+
+	return ub;
+error:
+	uwsgi_log("[uwsgi-bonjour] unable to generate dns name for %s\n", name);
+	exit(1);
+}
+
+static void register_cname(char *name, char *cname, int unique) {
+
+	DNSServiceFlags f = unique ? kDNSServiceFlagsUnique : kDNSServiceFlagsShared;
+
+	DNSRecordRef r = 0;
+	struct uwsgi_buffer *ub = to_dns(cname, strlen(cname));
+	DNSServiceErrorType error = DNSServiceRegisterRecord(bonjour_sdref, &r, f, 0, name, kDNSServiceType_CNAME, kDNSServiceClass_IN,
+		ub->pos, ub->buf, 60,
+                dnsCallback, NULL);
+
+	if (error) {
+		uwsgi_log("[uwsgi-bonjour] unable to register CNAME for %s, error code: %d\n", name, error);
+		exit(1);
+	}
+
+	uwsgi_buffer_destroy(ub);
+	uwsgi_log("[uwsgi-bonjour] registered record %s CNAME %s\n", name, cname);
+}
+
+static void register_a(char *name, char *addr, int unique) {
+
+        DNSServiceFlags f = unique ? kDNSServiceFlagsUnique : kDNSServiceFlagsShared;
+
+        DNSRecordRef r = 0;
+	uint32_t ip = inet_addr(addr);
+        DNSServiceErrorType error = DNSServiceRegisterRecord(bonjour_sdref, &r, f, 0, name, kDNSServiceType_A, kDNSServiceClass_IN,
+                4, &ip, 60,
+                dnsCallback, NULL);
+
+        if (error) {
+                uwsgi_log("[uwsgi-bonjour] unable to register A for %s, error code: %d\n", name, error);
+                exit(1);
+        }
+
+        uwsgi_log("[uwsgi-bonjour] registered record %s A %s\n", name, addr);
+}
+
+static void *bonjour_loop(void *arg) {
+	for(;;) {
+		DNSServiceErrorType error = DNSServiceProcessResult(bonjour_sdref);
+		if (error) {
+			uwsgi_log("[uwsgi-bonjour] error %d while processing result\n", error);
+		}
+	}
+	return NULL;
+}
+
+static void bonjour_init() {
+
+	if (!ubonjour.record) return;
+
+	DNSServiceErrorType error = DNSServiceCreateConnection(&bonjour_sdref);
+        if (error) {
+                uwsgi_log("[uwsgi-bonjour] unable to initialize DNS resolution, error code: %d\n", error);
+                exit(1);
+        }
+
+	char *myself = uwsgi.hostname;
+	if (!uwsgi_endswith(myself, ".local") && !uwsgi_endswith(myself, ".lan")) {
+		myself = uwsgi_concat2(uwsgi.hostname, ".local");
+	}
+
+	struct uwsgi_string_list *usl = NULL;
+	uwsgi_foreach(usl, ubonjour.record) {
+		char *b_name = NULL;
+		char *b_cname = NULL;
+		char *b_unique = NULL;
+		char *b_ip = NULL;
+		if (strchr(usl->value, '=')) {
+			if (uwsgi_kvlist_parse(usl->value, usl->len, ',', '=',
+			"name", &b_name,
+			"cname", &b_cname,
+			"unique", &b_unique,
+			"ip", &b_ip,
+			"a", &b_ip,
+			NULL)) {
+				uwsgi_log("[uwsgi-bonjour] invalid keyval syntax\n");
+				exit(1);
+			}
+
+			if (!b_name) {
+				uwsgi_log("[uwsgi-bonjour] you need to specify the name key to register a record\n");
+				exit(1);
+			}
+			
+			if (b_cname) {
+				register_cname(b_name, b_cname, b_unique ? 1 : 0);	
+			}
+			else if (b_ip) {
+				register_a(b_name, b_ip, b_unique ? 1 : 0);	
+			}
+			else {
+				register_cname(b_name, myself, b_unique ? 1 : 0);
+			}
+
+			if (b_name) free(b_name);
+			if (b_cname) free(b_cname);
+			if (b_unique) free(b_unique);
+			if (b_ip) free(b_ip);
+		}
+		else {
+			register_cname(usl->value, myself, 0);
+		}
+	}
+
+	if (myself != uwsgi.hostname)
+		free(myself);
+
+	// now start a pthread for managing bonjour
+	pthread_t t;
+        pthread_create(&t, NULL, bonjour_loop, NULL);
+}
+
+static void bonjour_close_fd() {
+	if (bonjour_sdref) {
+		close(DNSServiceRefSockFD(bonjour_sdref));
+	}
+}
+
+struct uwsgi_plugin bonjour_plugin = {
+	.name = "bonjour",
+	.options = bonjour_options,
+	.post_fork = bonjour_close_fd,
+	// we use .post_init instead of .init to avoid the mdsn socket to be closed on reloads
+	.post_init = bonjour_init,
+};
